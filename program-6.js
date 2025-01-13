09e8d3295aab8c1ee2c40adb1ef01e5194690f31
Tirth <tirth1943@gamil.com>
2025-01-13 15:41:44 +0530

    Completed JS test 06

--- program-6.js
+++ program-6.js
@@ -1,1 +1,19 @@
 // Write a JavaScript function that fetches data from an API and retries the request a specified number of times if it fails.
+async function fetchWithRetry(url, options = {}, retries = 99, delay = 2000) {
+  for (let i = 0; i < retries; i++) {
+    try {
+      const response = await fetch(url, options);
+      return await response.json();
+    } catch (error) {
+      if (i < retries - 1) {
+        console.log(`Retrying... (${i + 1})`);
+        await new Promise((resolve) => setTimeout(resolve, delay));
+      } else {
+        throw error;
+      }
+    }
+  }
+}
+fetchWithRetry("https://jsonplaceholder.typicode.com/todos/9")
+  .then((data) => console.log(data))
+  .catch((error) => console.error("Failed to fetch data:", error));
--- program-7.js
+++ program-7.js
@@ -1,1 +1,11 @@
 // Write a JavaScript program to implement a function that executes a given function repeatedly at a fixed interval using 'setInterval()'.
+function repeat(callback) {
+  setInterval(() => {
+    callback();
+  }, 1000);
+}
+
+repeat(()=>{
+    console.log('Hi There');
+    
+})
